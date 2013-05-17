2719f21cff6c9ab3d1ce7aecf39a2a891fddfe16
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-05-17 06:05:27 -0500

    cosmetic change #342904

--- src/main/java/net/visualillusionsent/minecraft/server/mod/plugin/dconomy/commands/InformationCommand.java
+++ src/main/java/net/visualillusionsent/minecraft/server/mod/plugin/dconomy/commands/InformationCommand.java
@@ -34,12 +34,12 @@ public final class InformationCommand extends dConomyCommand{
     public InformationCommand(){
         super(0);
         List<String> pre = new ArrayList<String>();
-        pre.add(center(MCChatForm.ORANGE + "---" + MCChatForm.LIGHT_GREEN + " dConomy Core v" + dCoBase.getRawVersion() + MCChatForm.ORANGE + " ---"));
+        pre.add(center(MCChatForm.CYAN + "---" + MCChatForm.LIGHT_GREEN + " d" + MCChatForm.ORANGE + "Conomy " + MCChatForm.PURPLE + "v" + dCoBase.getRawVersion() + MCChatForm.CYAN + " ---"));
         pre.add("$VERSION_CHECK$");
-        pre.add(MCChatForm.ORANGE + "Lead Developer:" + MCChatForm.LIGHT_GREEN + " DarkDiplomat");
-        pre.add(MCChatForm.ORANGE + "Contributers:" + MCChatForm.LIGHT_GREEN + " "); // If someone adds to dConomy, their name can go here
-        pre.add(MCChatForm.ORANGE + "Website:" + MCChatForm.LIGHT_GREEN + " http://wiki.visualillusionsent.net/dConomy");
-        pre.add(MCChatForm.ORANGE + "Issues:" + MCChatForm.LIGHT_GREEN + " https://github.com/Visual-Illusions/dConomy/issues");
+        pre.add(MCChatForm.CYAN + "Lead Developer:" + MCChatForm.LIGHT_GREEN + " DarkDiplomat");
+        pre.add(MCChatForm.CYAN + "Contributers:" + MCChatForm.LIGHT_GREEN + " "); // If someone adds to dConomy, their name can go here
+        pre.add(MCChatForm.CYAN + "Website:" + MCChatForm.LIGHT_GREEN + " http://wiki.visualillusionsent.net/dConomy");
+        pre.add(MCChatForm.CYAN + "Issues:" + MCChatForm.LIGHT_GREEN + " https://github.com/Visual-Illusions/dConomy/issues");
 
         // Next 3 lines should always remain at the end of the About
         pre.add(center("§aCopyright © 2011-2013 §2Visual §6I§9l§bl§4u§as§2i§5o§en§7s §2Entertainment"));
