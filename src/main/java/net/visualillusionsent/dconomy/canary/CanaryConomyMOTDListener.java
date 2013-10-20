71e275f8f0607852856d6c9e05a9e5c7bca1f792
darkdiplomat <darkdiplomat@visualillusionsent.net>
2013-10-20 05:40:25 -0500

    Canary MOTD Listener added for wallet balance retrieval

---
+++ src/main/java/net/visualillusionsent/dconomy/canary/CanaryConomyMOTDListener.java
@@ -0,0 +1,32 @@
+package net.visualillusionsent.dconomy.canary;
+
+import net.canarymod.Canary;
+import net.canarymod.api.entity.living.humanoid.Player;
+import net.canarymod.chat.MessageReceiver;
+import net.canarymod.motd.MOTDKey;
+import net.canarymod.motd.MessageOfTheDayListener;
+import net.visualillusionsent.dconomy.accounting.wallet.WalletHandler;
+import net.visualillusionsent.dconomy.api.dConomyUser;
+import net.visualillusionsent.dconomy.canary.api.Canary_User;
+import net.visualillusionsent.dconomy.dCoBase;
+
+/**
+ * Canary dConomy Message Of The Day Listener
+ *
+ * @author Jason (darkdiplomat)
+ */
+public final class CanaryConomyMOTDListener implements MessageOfTheDayListener {
+
+    public CanaryConomyMOTDListener(CanarydConomy cdConomy) {
+        Canary.motd().registerMOTDListener(this, cdConomy, false);
+    }
+
+    @MOTDKey(key = "{wallet.balance}")
+    public String wallet_balance(MessageReceiver msgrec) {
+        return String.valueOf(WalletHandler.getWallet(asUser(msgrec)).getBalance());
+    }
+
+    private dConomyUser asUser(MessageReceiver msgrec) {
+        return msgrec instanceof Player ? new Canary_User((Player) msgrec) : dCoBase.getServer();
+    }
+}
