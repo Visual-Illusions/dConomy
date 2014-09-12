72eab2c4d510c8109d7b6766c720a5520c77c033
darkdiplomat <darkdiplomat@visualillusionsent.net>
2014-09-12 07:34:55 -0500

    making sure everything is compiled, updated, and included in the jar

--- src/main/java/net/visualillusionsent/dconomy/api/dConomyUser.java
+++ src/main/java/net/visualillusionsent/dconomy/api/dConomyUser.java
@@ -25,17 +25,6 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* Copyright 2013 Visual Illusions Entertainment.
- * This file is part of Visual Illusions Minecraft Mod Interface Library (VI-MCMIL).
- * VI-MCMIL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * VI-MCMIL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with VI-MCMIL.
- * If not, see http://www.gnu.org/licenses/gpl.html */
 package net.visualillusionsent.dconomy.api;
 
 import java.util.UUID;
--- src/main/java/net/visualillusionsent/dconomy/data/DataSourceException.java
+++ src/main/java/net/visualillusionsent/dconomy/data/DataSourceException.java
@@ -25,25 +25,6 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-/* 
- * Copyright 2012 - 2013 Visual Illusions Entertainment.
- *  
- * This file is part of Realms.
- *
- * Realms is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * Realms is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with Realms.
- * If not, see http://www.gnu.org/licenses/gpl.html
- * 
- * Source Code availible @ https://github.com/darkdiplomat/Realms
- */
 package net.visualillusionsent.dconomy.data;
 
 import java.sql.SQLException;
